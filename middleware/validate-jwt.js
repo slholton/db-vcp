4a7525b285bd49cad9ff945552eaf4fc3047bfeb
Shavonne Holton <shavonne.holton@gmail.com>
2021-09-14 07:09:45 -0700

    updated validate-jwt

--- middleware/validate-jwt.js
+++ middleware/validate-jwt.js
@@ -9,7 +9,7 @@ const validateJWT = async (req, res, next) => {
         req.headers.authorization.includes("Bearer")
     ) {
         const { authorization } = req.headers;
-        console.log("authorization -->", authorization);
+
         const payload = authorization
         ? jwt.verify(
             authorization.includes("Bearer")
@@ -19,14 +19,10 @@ const validateJWT = async (req, res, next) => {
         )
         : undefined;
 
-        console.log("payload -->", payload);
-
         if (payload) {
             let foundUser = await User.findOne({ where: { id: payload.id } });
-            console.log("foundUser -->", foundUser);
 
             if (foundUser) {
-                console.log("request -->", req)
                 req.user = foundUser;
                 next();
             } else {
