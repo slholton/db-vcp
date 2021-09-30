eb58a705aeaf96885435f4d7e0a0feac426d2c89
Shavonne Holton <shavonne.holton@gmail.com>
2021-09-30 16:07:02 -0700

    updated db files

--- db.js
+++ db.js
@@ -1,14 +1,8 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(
-    process.env.DB_DBNAME,
-    process.env.DB_USER, 
-    process.env.DB_PASS, 
-    {
-        host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres'
-    }
-)
+    })
 
 async function syncDb(sequelize, options){
     const { force, alter } = options
